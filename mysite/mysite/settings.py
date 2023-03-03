2cc1d621b2b0b15d1f5232ba1d61e90b49adb50c
DamirAboba <damirplaneta6526@gmail.com>
2023-03-03 20:53:07 +0600

    Initial commit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'products'
 
 ]
@@ -122,6 +121,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # This is now a string
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
